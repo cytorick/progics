5eb5e4470c9776858a9658a2a781e06578401e34
Rick Visser <rickvisser99@gmail.com>
2023-01-21 17:40:56 +0100

    Small fixes

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
+{{--        <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">--}}
 
         <title>{{ config('app.name', 'PROGICS') }}</title>
 
@@ -15,22 +15,48 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="h-full">
-    <div class="min-h-full bg-white py-16 px-6 sm:py-24 md:grid md:place-items-center lg:px-8">
-        <div class="mx-auto max-w-max">
-            <main class="sm:flex">
-                <img src="{{ asset('images/progics-logo.svg') }}" alt="Progics Logo" class="h-36">
-                <div class="sm:ml-6">
-                    <div class="sm:border-l sm:border-gray-200 sm:pl-6">
-                        <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">Onze nieuwe website is onderweg...</h1>
-                        <p class="mt-1 text-base text-gray-500"></p>
+    <div class="flex min-h-full flex-col bg-white lg:relative">
+        <div class="flex flex-grow flex-col">
+            <main class="flex flex-grow flex-col bg-white">
+                <div class="mx-auto flex w-full max-w-7xl flex-grow flex-col px-6 lg:px-8">
+                    <div class="flex-shrink-0 pt-10 sm:pt-16">
+                        <a href="/" class="inline-flex">
+                            <span class="sr-only">Your Company</span>
+                            <img class="h-12 w-auto hidden sm:block" src="{{ asset('images/progics-icon.png') }}" alt="">
+                        </a>
                     </div>
-                    <div class="mt-10 flex space-x-3 sm:border-l sm:border-transparent sm:pl-6">
-                        <a href="mailto:info@progics.nl" class="inline-flex items-center rounded-md border border-transparent bg-progics100 px-4 py-2 text-sm font-medium text-white hover:bg-progics focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Neem contact met ons op!</a>
+                    <div class="sm:hidden my-auto flex-shrink-0 py-16 sm:py-28 text-center justify-items-center justify-center items-center">
+                        <img class="h-20 w-auto relative mb-20 mx-auto" src="{{ asset('images/progics-logo.svg') }}" alt="PROGICS logo">
+                        <p class="text-base font-semibold text-progics">Binnenkort</p>
+                        <h1 class="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">Nog even wachten...</h1>
+                        <p class="mt-2 text-base text-gray-500">En dan is de nieuwe website van Progics klaar!</p>
+                        <div class="mt-6">
+                            <a href="mailto:info@progics.nl" class="text-base font-medium text-progics hover:text-progics100">
+                                Stuur ons een email
+                                <span aria-hidden="true"> &rarr;</span>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="hidden sm:block my-auto flex-shrink-0 py-16 sm:py-28">
+                        <p class="text-base font-semibold text-progics">Binnenkort</p>
+                        <h1 class="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">Nog even wachten...</h1>
+                        <p class="mt-2 text-base text-gray-500">En dan is de nieuwe website van Progics klaar!</p>
+                        <div class="mt-6">
+                            <a href="mailto:info@progics.nl" class="text-base font-medium text-progics hover:text-progics100">
+                                Stuur ons een email
+                                <span aria-hidden="true"> &rarr;</span>
+                            </a>
+                        </div>
                     </div>
                 </div>
             </main>
+
+        </div>
+        <div class="hidden lg:absolute lg:inset-y-0 lg:right-0 lg:block lg:w-1/2">
+            <img class="absolute inset-0 h-full w-full object-cover" src="{{ asset('images/Middel 3.png') }}" alt="">
         </div>
     </div>
+
 
     </body>
 </html>
